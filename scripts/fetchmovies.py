52e3d3d60d925faf79a131413372e87607fe514a
geekodour <hrishikeshbman@gmail.com>
2019-02-14 17:18:43 +0530

    wip

--- scripts/fetchmovies.py
+++ scripts/fetchmovies.py
@@ -1,24 +1,44 @@
 import csv
 from pymongo import MongoClient
 
-# connect to the mongodb and create both database
+
+# database inits
 client = MongoClient('localhost', 27017)
-tempdb = client['tempdb']
 moviedb = client['moviedb']
 
-# create tempdb collections
+# tempdb
+tempdb = client['tempdb']
+tempdb['movies'].drop()
 tempmovies = tempdb['movies']
 
-with open('data/movies.csv', newline='') as moviefile:
-    moviereader = csv.reader(moviefile, delimiter=',', quotechar='|')
-    for row in moviereader[1:]
-        print(row)
-        tempmovies.insert_one(
-            {
-                'name': row[1]
-            }
-        )
+# csv inits
+linkfile = open('data/links.csv')
+moviefile = open('data/movies.csv')
 
+moviereader = csv.reader(moviefile, delimiter=',', quotechar='"')
+linkreader = csv.reader(linkfile, delimiter=',', quotechar='"')
+
+next(moviereader, None) # skip header row
+next(linkreader, None)
+
+for row in moviereader:
+   linkinfo = next(linkreader, None)
+   tempmovies.insert_one(
+    {
+      'year': row[1][-5:-1],
+      'name': row[1][:-7],
+      'genere': row[2].split('|'),
+      'link': linkinfo[1]
+    }
+   )
+
+# get 10 unique generes
+# get 20 movies from each genere
+# use requests
+
+tempdb['movies'].drop()
+linkfile.close()
+moviefile.close()
 client.close()
 
 #- title
